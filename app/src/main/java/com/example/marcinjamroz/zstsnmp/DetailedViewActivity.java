9a73289d6de5819946441a923246046f6fd6f193
Marcin <marcin919@op.pl>
2017-01-16 17:20:26 +0100

    działające gniazdka i Async

--- app/src/main/java/com/example/marcinjamroz/zstsnmp/DetailedViewActivity.java
+++ app/src/main/java/com/example/marcinjamroz/zstsnmp/DetailedViewActivity.java
@@ -1,17 +1,25 @@
 package com.example.marcinjamroz.zstsnmp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Space;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class DetailedViewActivity extends AppCompatActivity {
+
+    int oidID;
 
 
     @Override
@@ -23,10 +31,12 @@ public class DetailedViewActivity extends AppCompatActivity {
 
         String value = intent.getStringExtra("MIB");
 
+        setTitle(value);
+
         TextView oid = new TextView(this);
         TextView description = new TextView(this);
 
-        int oidID = this.getResources().getIdentifier(value+"OID","string",this.getPackageName());
+         oidID = this.getResources().getIdentifier(value+"OID","string",this.getPackageName());
         int descriptionID = this.getResources().getIdentifier(value+"Description","string",this.getPackageName());
 
         oid.setText(oidID);
@@ -40,14 +50,33 @@ public class DetailedViewActivity extends AppCompatActivity {
         viewGroup.addView(oid);
         viewGroup.addView(description);
 
-        try {
-            Socket socket = new Socket("10.78.17.22",5555);
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF(getResources().getString(oidID));
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+
+    }
+
+    protected void getOID(View view){
+        new sendAndReceive().execute(oidID);
+    }
+
+
+
+    public class sendAndReceive extends AsyncTask<Integer,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            try {
+                Socket socket = new Socket("192.168.43.48",8888);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(getResources().getString(params[0]));
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
+
 
     }
 
